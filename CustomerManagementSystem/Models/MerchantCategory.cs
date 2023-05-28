80d1fe0ec7f1542c720380d73194d65655a5930a
ama91bh <treke.studio@gmail.com>
2023-05-28 21:47:13 +0300

    Set up the project and created a model for the MerchantCategory table to track the types of merchants that can be added. Installed necessary packages such as the DbContext, tools, and Entity Framework. Created a data file to store changes that will be pushed to the database.

---
+++ CustomerManagementSystem/Models/MerchantCategory.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerManagementSystem.Models
+{
+    public class MerchantCategory
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
